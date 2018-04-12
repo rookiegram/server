d6bba6204b2771e7b7c461ae27040ce31b3c794b
Wikanyaa <Mac@Macbooknyaa.localdomain>
2018-04-12 19:48:41 +0700

    feature upload

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
+const multer = require('multer')
+
+const uploadMidleware = require('../middleware/upload')
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+const uploaderMem = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024
+  }
+})
+
+router.post('/upload', uploaderMem.single('picture'), uploadMidleware.upload, (req, res) => {
+  res.status(201).json({
+    message: 'file berhasil di-upload dengan url ',
+    data: req.imageURL
+  })
+})
 
 module.exports = router;
